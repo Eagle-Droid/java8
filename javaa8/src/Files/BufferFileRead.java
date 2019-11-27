00776d5060617abb3fd799957942c2bd950e9a75
stecer-byte <57720912+stecer-byte@users.noreply.github.com>
2019-11-27 18:40:02 +0200

    Add files via upload

---
+++ javaa8/src/Files/BufferFileRead.java
@@ -0,0 +1,22 @@
+package Files;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferFileRead {
+    public static void main(String[] args) {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader("src/files/FileRead.java"));
+            String line;
+            while((line=file.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ javaa8/src/Files/FileWrite.java
@@ -0,0 +1,16 @@
+package Files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWrite {
+    public static void main(String[] args) {
+        try {
+            FileWriter file = new FileWriter("test.txt");
+            file.write("Hello Java!");
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
